60b32fb864ff9e61ae2aea708717b1e1f505afa5
Mika Vesasto <mika.vesasto@gmail.com>
2026-06-11 09:03:20 +0300

    Add missing color variables and use in components. Add same styles to TextField c-text-field than others have

--- src/components/dynamic/TextField.vue
+++ src/components/dynamic/TextField.vue
@@ -18,3 +18,14 @@ defineEmits<{
     @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
   />
 </template>
+
+<style lang="scss" scoped>
+c-text-field {
+  --c-text-field-background-color: var(--color-white);
+  --c-text-field-text-color: var(--color-text);
+  --c-text-field-label-color: var(--color-white);
+  --c-text-field-placeholder-color: #9e9e9e;
+  --c-text-field-active-color: var(--color-pink);
+  --c-text-field-inactive-color: rgba(255, 255, 255, 0.6);
+}
+</style>
