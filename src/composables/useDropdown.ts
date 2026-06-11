8d4fde405c27a8a34fc1bb45e579ae407e50421a
Mika Vesasto <mika.vesasto@gmail.com>
2026-06-11 09:05:59 +0300

    Reusable dropdown functions composable

---
+++ src/composables/useDropdown.ts
@@ -0,0 +1,45 @@
+import { nextTick, ref } from 'vue'
+import type { Ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+
+interface UseDropdownOptions {
+  searchRef: Readonly<Ref<HTMLInputElement | null>>
+  triggerRef: Readonly<Ref<HTMLButtonElement | null>>
+}
+
+interface UseDropdownReturn {
+  isOpen: Ref<boolean>
+  containerRef: Ref<HTMLElement | null>
+  openDropdown: () => void
+  closeDropdown: (refocusTrigger?: boolean) => void
+  toggleDropdown: () => void
+}
+
+export function useDropdown({ searchRef, triggerRef }: UseDropdownOptions): UseDropdownReturn {
+  const isOpen = ref(false)
+  const containerRef = ref<HTMLElement | null>(null)
+
+  function openDropdown() {
+    isOpen.value = true
+    nextTick(() => searchRef.value?.focus())
+  }
+
+  function closeDropdown(refocusTrigger = true) {
+    isOpen.value = false
+    if (refocusTrigger) {
+      nextTick(() => triggerRef.value?.focus())
+    }
+  }
+
+  function toggleDropdown() {
+    if (isOpen.value) {
+      closeDropdown()
+    } else {
+      openDropdown()
+    }
+  }
+
+  onClickOutside(containerRef, () => closeDropdown(false))
+
+  return { isOpen, containerRef, openDropdown, closeDropdown, toggleDropdown }
+}
