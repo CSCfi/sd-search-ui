6441d4ed16d559a03d9e7ec4379aeea069265fea
Mika Vesasto <mika.vesasto@gmail.com>
2026-06-12 14:06:01 +0300

    Add tanstack query + eslint config for tanstack. Setup proxy /api for vite.

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,28 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     vue({
       template: {
         compilerOptions: {
           isCustomElement: (tag) => tag.startsWith('c-'),
-        }
-      }
+        },
+      },
     }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   css: {
