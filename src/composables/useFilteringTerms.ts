72c126a71caf5d5193d50e9155b479d891cef10b
Mika Vesasto <mika.vesasto@gmail.com>
2026-06-12 15:28:28 +0300

    Whitelist > Blacklist

--- src/composables/useFilteringTerms.ts
+++ src/composables/useFilteringTerms.ts
@@ -1,20 +1,7 @@
 import { useQuery } from '@tanstack/vue-query'
 import { fetchFilteringTerms } from '@/services/filteringTermsApi'
 
-// Backend returns fields that is not meant to be used in UI currently. Whitelist visible fields.
-const VISIBLE_FIELD_IDS = new Set([
-  'dataset_description',
-  'animal_species',
-  'anatomical_site',
-  'sex',
-  'age_at_extraction',
-  'fixation_type',
-  'block_preparation',
-  'specimen_type',
-  'staining_procedure',
-  'staining_substance',
-  'staining_target',
-])
+const HIDDEN_FIELD_IDS = new Set(['dataset_title'])
 
 export function useFilteringTerms() {
   return useQuery({
@@ -24,7 +11,7 @@ export function useFilteringTerms() {
     select: (data) => ({
       ...data,
       response: {
-        filteringTerms: data.response.filteringTerms.filter((f) => VISIBLE_FIELD_IDS.has(f.id)),
+        filteringTerms: data.response.filteringTerms.filter((f) => !HIDDEN_FIELD_IDS.has(f.id)),
       },
     }),
   })
